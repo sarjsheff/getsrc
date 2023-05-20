3e88bf11cb894df5daef203e035b7189b041f4e3
sarjsheff <sarjsheff@yandex.ru>
2023-05-20 10:20:32 +0300

    +css

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 npx tailwindcss -i ./main.css -o ./static/css/getsrc.css
 
-echo '{{define "css"}}<link href="/css/getsrc.css#'`md5 -q static/css/getsrc.css`'" rel="stylesheet">{{end}}' > tmpl/gen.go.html
+echo '{{define "css"}}<link href="/css/getsrc.css?'`md5 -q static/css/getsrc.css`'" rel="stylesheet">{{end}}' > tmpl/gen.go.html
